04cb1afcf4ca8b63b41683e47307b100f514306b
varsha-kotegar <varshakotegar26@gmail.com>
2025-07-14 11:13:16 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -31,6 +32,8 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// in production, there's no localhost so we have to make this dynamic
+// In production, there's no localhost so we need to make this dynamic
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 const api = axios.create({
